632eaf0dd89b4d49e6b40a581d48e5feb9c5982c
Cleo Alcanzo <94949037+Azerux123@users.noreply.github.com>
2023-10-18 21:34:33 +0800

    added animation on scroll

--- cleo.js
+++ cleo.js
@@ -8,3 +8,17 @@ window.onscroll = function () {
   let progressHeight = (window.pageYOffset / totalHeight) * 100;
   progress.style.height = progressHeight + "%";
 }
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
